ec971fa4f16d9d0d3c9f2698649ac9e64db1b561
Tor Sinclair <tor@romanesco.se>
2016-03-07 11:45:08 -0800

    Pass all tests for HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
+
+// set honeymakerbee.prototype to look through to bee
+HoneyMakerBee.prototype = new Bee();
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--;
+};
+
+// set constructor so we can override stuff using our honeymakebee
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
