acbba91129999c4aafe07f358dd92d66d8ec5694
Tor Sinclair <tor@romanesco.se>
2016-03-07 11:44:22 -0800

    Pass all tests for Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+// Set Prototype as a new instance of Grub (the parent)
+Bee.prototype = new Grub();
+// Set constructor = Bee to 'control' flow
+Bee.prototype.constructor = Bee;
+
 
