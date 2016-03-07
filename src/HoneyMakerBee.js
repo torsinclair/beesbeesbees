861d3af323f2743b736e7932b1f99d89c808f35d
Tor Sinclair <tor@romanesco.se>
2016-03-07 14:55:59 -0800

    Amend the delegation method to Object.create()

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,11 +1,12 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
   this.age = 10;
   this.job = 'make honey';
   this.honeyPot = 0;
 };
 
 // set honeymakerbee.prototype to look through to bee
-HoneyMakerBee.prototype = new Bee();
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
 
 HoneyMakerBee.prototype.makeHoney = function(){
   this.honeyPot++;
