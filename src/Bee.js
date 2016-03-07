534f93283d609df733506e842bf9cb9aab7ac620
Tor Sinclair <tor@romanesco.se>
2016-03-07 14:54:41 -0800

    Amend the delegation method to Object.create()

--- src/Bee.js
+++ src/Bee.js
@@ -1,12 +1,23 @@
 var Bee = function() {
+  Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
   this.job = 'keep on growing';
 };
 
 // Set Prototype as a new instance of Grub (the parent)
-Bee.prototype = new Grub();
-// Set constructor = Bee to 'control' flow
+Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Bee;
+
+// ^^^THIS IS WRONG^^^
+
+//Bee.prototype = Object.create(Grub.prototype);
+
+// Set constructor = Bee to 'control' flow
+// override the prototype property
+// constructor is a circular reference that helps the
+// instance work out what its prototype is
+
+
 
 
