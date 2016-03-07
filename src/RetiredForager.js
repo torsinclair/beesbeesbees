37038578611fa7d81922f1ae40ddc2505f1a3ae5
Tor Sinclair <tor@romanesco.se>
2016-03-07 12:01:00 -0800

    Complete RetiredForagerBee tests

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,21 @@
 var RetiredForagerBee = function() {
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+// create the subclass relationship
+RetiredForagerBee.prototype = new ForagerBee();
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+
+RetiredForagerBee.prototype.forage = function(){
+  var str = "I am too old, let me play cards instead";
+  return str;
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure){
+  this.treasureChest.push(treasure);
+};
